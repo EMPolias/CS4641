d22a6e76f3d70eff57ea60fa5931cd312c11ebe6
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-01-29 14:53:29 -0500

    separate cifar dataset into test and training data

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,23 +1,32 @@
 """Methods for importing the datasets in python-usable format."""
 
-def cifar_train():
+def cifar_batches():
     import cPickle
-    data = {}
+    data = []
     for batch_num in range(1, 6):
         with open('datasets/cifar-10-batches-py/data_batch_' + str(batch_num), 'rb') as f:
-            data['batch_' + str(batch_num)] = cPickle.load(f)
+            data.append(cPickle.load(f))
     return data
 
-def cifar_test():
+def cifar_test_batch():
     import cPickle
     with open('datasets/cifar-10-batches-py/test_batch', 'rb') as f:
         data = cPickle.load(f)
     return data
 
 def cifar():
-    data = cifar_train()
-    data['test_batch'] = cifar_test()
-    return data
+    data_ = cifar_batches() + [cifar_test_batch()]
+    join_batches = lambda r1, r2, cat: [x for sublist in [batch[cat] for batch in data_[r1:r2]] for x in sublist]
+    return {
+            'train': {
+                'data': join_batches(0, 4, 'data'),
+                'labels': join_batches(0, 4, 'labels')
+                },
+            'test': {
+                'data': join_batches(4, 6, 'data'),
+                'labels': join_batches(4, 6, 'labels')
+                }
+            }
 
 
 def read_sentiment_data(f_name):
