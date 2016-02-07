f0ee54c17c4d3bee352c7e6e6036ef986ba96474
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-02-07 11:34:18 -0500

    datasets for nns

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,8 @@ from collections import Counter
 
 import random
 import numpy as np
+from sklearn import preprocessing
+from pybrain.datasets.classification import ClassificationDataSet
 
 
 def cifar_batches():
@@ -20,26 +22,52 @@ def cifar_test_batch():
         data = cPickle.load(f)
     return data
 
-def cifar():
+def cifar(one_hot=False, ten_percent=False):
     data_ = cifar_batches() + [cifar_test_batch()]
     join_batches = lambda r1, r2, cat: [x for sublist in [batch[cat] for batch in data_[r1:r2]] for x in sublist]
-    train_data, train_labels = shuffle_data(join_batches(0, 4, 'data'), join_batches(0, 4, 'labels'))
-    test_data, test_labels = shuffle_data(join_batches(4, 6, 'data'), join_batches(4, 6, 'labels'))
+    train_data, train_labels = shuffle_data(join_batches(0, 4, 'data'), join_batches(0, 4, 'labels'), ten_percent=ten_percent)
+    test_data, test_labels = shuffle_data(join_batches(4, 6, 'data'), join_batches(4, 6, 'labels'), ten_percent=ten_percent)
 
-    return {
+    data = {
             'train': {
-                'data': train_data,
-                'labels': train_labels
+                'data': preprocessing.normalize(train_data),
+                'labels': list(train_labels)
                 },
             'test': {
-                'data': test_data,
-                'labels': test_labels
+                'data': preprocessing.normalize(test_data),
+                'labels': list(test_labels)
                 }
             }
 
-def shuffle_data(data, labels):
+    if one_hot:
+        for set_ in ['train', 'test']:
+            for i in xrange(len(data[set_]['labels'])):
+                klass = data[set_]['labels'][i]
+                one_hot_vec = [0] * 10
+                one_hot_vec[klass] = 1
+                data[set_]['labels'][i] = one_hot_vec
+
+    return data
+
+def cifar_nn(offset=None):
+    data_ = cifar(one_hot=True, ten_percent=False)
+    x_dim = len(data_['train']['data'][0])
+    data = ClassificationDataSet(x_dim, 10)
+    if offset:
+        max_sample = offset
+    else:
+        max_sample = len(data_['train']['data'])
+    for i in xrange(max_sample):
+        data.addSample(data_['train']['data'][i], data_['train']['labels'][i])
+    data_['train_nn'] = data
+    return data_
+
+
+def shuffle_data(data, labels, ten_percent=False):
     z = zip(data, labels)
     random.shuffle(z)
+    if ten_percent:
+        z = z[:int(len(z) * 0.1)]
     return zip(*z)
 
 
@@ -95,3 +123,17 @@ def sentiment(bag_size=100):
                 'labels': labels[offset:]
                 }
             }
+
+def sentiment_nn(bag_size=100, offset=None):
+    data_ = sentiment(bag_size)
+    x_dim = len(data_['train']['data'][0])
+    data = ClassificationDataSet(x_dim, 1)
+    if offset:
+        max_sample = offset
+    else:
+        max_sample = len(data_['train']['data'])
+    for i in xrange(max_sample):
+        data.addSample(data_['train']['data'][i], [data_['train']['labels'][i]])
+    data_['train_nn'] = data
+    return data_
+
