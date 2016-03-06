c4f5f48d2be5fef8e36ddfb35cc68edb81aaaf52
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-06 17:37:31 -0500

    modify Sentiment dataset reader

--- P2/SentimentTest.java
+++ P2/SentimentTest.java
@@ -3,31 +3,47 @@ import java.lang.StringBuilder;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.Scanner;
 
 
 class SentimentTest {
 
-  private static Instance[] initializeInstances() {
-    ArrayList<Instance> data = new ArrayList<Instance>();
+  public static void main(String[] args) {
+      System.out.println(initializeInstances("5")[0]);
+  } 
 
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+  private static Instance[] initializeInstances(String bagSize) {
+      double[][][] attributes = new double[3000][][]; // 3000 instances
 
-      String review = "";
-      String line;
+      try {
+          BufferedReader br = new BufferedReader(new FileReader(new File("datasets/bag" + bagSize + ".data")));
 
-      while ((line = br.readLine()) != null) {
-        if (line[line.length() - 2] == '0' || line[line.length() - 2] == '1') {
-          review += line.substring(0, line.length() - 2);
-          Character label = line[line.length() - 2];
-          Instance
-        } else {
-          review += line;
-        }
+          for(int i = 0; i < attributes.length; i++) {
+              Scanner scan = new Scanner(br.readLine());
+              scan.useDelimiter(", ");
+
+              attributes[i] = new double[2][];
+              attributes[i][0] = new double[Integer.parseInt(bagSize)]; // bagSize attributes
+              attributes[i][1] = new double[1];
+
+              for(int j = 0; j < Integer.parseInt(bagSize); j++)
+                  attributes[i][0][j] = Double.parseDouble(scan.next());
+
+              attributes[i][1][0] = Double.parseDouble(scan.next());
+          }
       }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+      catch(Exception e) {
+          e.printStackTrace();
+      }
+
+      Instance[] instances = new Instance[attributes.length];
+
+      for(int i = 0; i < instances.length; i++) {
+          instances[i] = new Instance(attributes[i][0]);
+          instances[i].setLabel(new Instance(attributes[i][1][0] < .5 ? 0 : 1));
+      }
+
+      return instances;
   }
 
 }
