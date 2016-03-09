71b11848c9bf713f494ce5187fb6c97639c8dfad
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-09 08:11:21 -0500

    add graphs for new experiments

--- P2/plot.py
+++ P2/plot.py
@@ -1,22 +1,23 @@
 import matplotlib.pyplot as plt
 from temp import * # where the variables are defined
 
+datasetPercentage = [1, 2, 3, 5, 8, 10, 15]
 
 plt.figure()
-plt.title('Simulated Annealing: Performance x Training Set Size\n(15000 training iterations)')
+plt.title('Randomized Hill Climbing: Performance x Number of Restarts\n(5500 training iterations)')
 plt.plot(datasetPercentage, test_error, '-', label='test error')
 plt.plot(datasetPercentage, train_error, '-', label='train error')
 plt.legend()
-plt.xlabel('Percentage of Training Set')
+plt.xlabel('Number of Restarts')
 plt.ylabel('Sum of Squares Error')
 plt.show()
 
 
 plt.figure()
-plt.title('Simulated Annealing: Performance x Training Set Size\n(15000 training iterations)')
-plt.plot(datasetPercentage, testing_time, '-', label='test time')
-plt.plot(datasetPercentage, training_time, '-', label='train time')
+plt.title('Randomized Hill Climbing: Train/Test time x Number of Restarts\n(5500 training iterations)')
+plt.plot(datasetPercentage, test_time, '-', label='test time')
+plt.plot(datasetPercentage, train_time, '-', label='train time')
 plt.legend()
-plt.xlabel('Percentage of Training Set')
+plt.xlabel('Number of Restarts')
 plt.ylabel('Train/Test time')
 plt.show()
