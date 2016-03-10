15daf978f1d4cdbdb15558fed6cf62a228e61c9a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-10 18:32:21 -0500

    add traveling salesman problem experiment, runs, and plots

--- P2/plot3.py
+++ P2/plot3.py
@@ -2,15 +2,31 @@ import matplotlib.pyplot as plt
 from temp import *
 
 
-maxIter = 1000
+problem_name = 'Traveling Salesman'
+
+# MIMIC has interesting results if you plot all iterations
+maxIter = len(mimic)
 plt.figure()
-plt.title('Four Peaks : Iteration x Fitness')
-plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '.', label='RHC')
-plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '.', label='SimA')
-plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '.', label='GenAl')
-plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '.', label='MIMIC')
+plt.title(problem_name + ' : Iteration x Fitness')
+plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '-', label='RHC')
+plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '-', label='SimA')
+plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '-', label='GenAl')
+plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '-', label='MIMIC')
 plt.legend(loc=4)
-plt.xlabel('Iteration')
+plt.xlabel('Iteration (*1000)')
+plt.ylabel('Fitness')
+plt.show()
+
+
+maxIter = 50
+plt.figure()
+plt.title(problem_name + ' : Iteration x Fitness')
+plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '-', label='RHC')
+plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '-', label='SimA')
+plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '-', label='GenAl')
+plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '-', label='MIMIC')
+plt.legend(loc=4)
+plt.xlabel('Iteration (*1000)')
 plt.ylabel('Fitness')
 plt.show()
 
@@ -25,11 +41,11 @@ mimic_times = filter(lambda x: x < max_time, map(lambda x: x[0], mimic))
 mimic_fitns = map(lambda x: x[1], mimic)[:len(mimic_times)]
 
 plt.figure()
-plt.title('Four Peaks : Time x Fitness')
-plt.plot(rhc_times, rhc_fitns, '.', label='RHC')
-plt.plot(sima_times, sima_fitns, '.', label='SimA')
-plt.plot(genal_times, genal_fitns, '.', label='GenAl')
-plt.plot(mimic_times, mimic_fitns, '.', label='MIMIC')
+plt.title(problem_name + ' : Time x Fitness')
+plt.plot(rhc_times, rhc_fitns, '-', label='RHC')
+plt.plot(sima_times, sima_fitns, '-', label='SimA')
+plt.plot(genal_times, genal_fitns, '-', label='GenAl')
+plt.plot(mimic_times, mimic_fitns, '-', label='MIMIC')
 plt.legend(loc=4)
 plt.xlabel('Time')
 plt.ylabel('Fitness')
@@ -37,12 +53,12 @@ plt.show()
 
 
 plt.figure()
-plt.title('Four Peaks : Time (log) x Fitness')
+plt.title(problem_name + ' : Time (log) x Fitness')
 plt.xscale('log')
-plt.plot(rhc_times, rhc_fitns, '.', label='RHC')
-plt.plot(sima_times, sima_fitns, '.', label='SimA')
-plt.plot(genal_times, genal_fitns, '.', label='GenAl')
-plt.plot(mimic_times, mimic_fitns, '.', label='MIMIC')
+plt.plot(rhc_times, rhc_fitns, '-', label='RHC')
+plt.plot(sima_times, sima_fitns, '-', label='SimA')
+plt.plot(genal_times, genal_fitns, '-', label='GenAl')
+plt.plot(mimic_times, mimic_fitns, '-', label='MIMIC')
 plt.legend(loc=2)
 plt.xlabel('Time')
 plt.ylabel('Fitness')
