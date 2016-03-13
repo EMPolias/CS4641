b965f855ff79d93c03ea0f3e0ed6d18a986bdd3c
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-13 15:56:29 -0400

    add plots of performance for pretty graphs

--- P2/plot3.py
+++ P2/plot3.py
@@ -2,35 +2,36 @@ import matplotlib.pyplot as plt
 from temp import *
 
 
-problem_name = 'Traveling Salesman'
+problem_name = 'Pretty Graphs'
 
 # MIMIC has interesting results if you plot all iterations
-maxIter = len(mimic)
+# maxIter = len(mimic)
+maxIter = 15
 plt.figure()
 plt.title(problem_name + ' : Iteration x Fitness')
 plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '-', label='RHC')
 plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '-', label='SimA')
 plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '-', label='GenAl')
-plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '-', label='MIMIC')
+plt.plot(range(len(mimic)), map(lambda x: x[1], mimic), '-', label='MIMIC')
 plt.legend(loc=4)
-plt.xlabel('Iteration (*1000)')
+plt.xlabel('Iteration (*100)')
 plt.ylabel('Fitness')
 plt.show()
 
 
-maxIter = 50
-plt.figure()
-plt.title(problem_name + ' : Iteration x Fitness')
-plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '-', label='RHC')
-plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '-', label='SimA')
-plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '-', label='GenAl')
-plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '-', label='MIMIC')
-plt.legend(loc=4)
-plt.xlabel('Iteration (*1000)')
-plt.ylabel('Fitness')
-plt.show()
+# maxIter = 50
+# plt.figure()
+# plt.title(problem_name + ' : Iteration x Fitness')
+# plt.plot(range(maxIter), map(lambda x: x[1], rhc)[:maxIter], '-', label='RHC')
+# plt.plot(range(maxIter), map(lambda x: x[1], sima)[:maxIter], '-', label='SimA')
+# plt.plot(range(maxIter), map(lambda x: x[1], genal)[:maxIter], '-', label='GenAl')
+# plt.plot(range(maxIter), map(lambda x: x[1], mimic)[:maxIter], '-', label='MIMIC')
+# plt.legend(loc=4)
+# plt.xlabel('Iteration (*100)')
+# plt.ylabel('Fitness')
+# plt.show()
 
-max_time = 100
+max_time = 200
 rhc_times = filter(lambda x: x < max_time, map(lambda x: x[0], rhc))
 rhc_fitns = map(lambda x: x[1], rhc)[:len(rhc_times)]
 sima_times = filter(lambda x: x < max_time, map(lambda x: x[0], sima))
@@ -42,8 +43,8 @@ mimic_fitns = map(lambda x: x[1], mimic)[:len(mimic_times)]
 
 plt.figure()
 plt.title(problem_name + ' : Time x Fitness')
-plt.plot(rhc_times, rhc_fitns, '-', label='RHC')
-plt.plot(sima_times, sima_fitns, '-', label='SimA')
+plt.plot(rhc_times, rhc_fitns, '.', label='RHC')
+plt.plot(sima_times, sima_fitns, '.', label='SimA')
 plt.plot(genal_times, genal_fitns, '-', label='GenAl')
 plt.plot(mimic_times, mimic_fitns, '-', label='MIMIC')
 plt.legend(loc=4)
@@ -55,8 +56,8 @@ plt.show()
 plt.figure()
 plt.title(problem_name + ' : Time (log) x Fitness')
 plt.xscale('log')
-plt.plot(rhc_times, rhc_fitns, '-', label='RHC')
-plt.plot(sima_times, sima_fitns, '-', label='SimA')
+plt.plot(rhc_times, rhc_fitns, '.', label='RHC')
+plt.plot(sima_times, sima_fitns, '.', label='SimA')
 plt.plot(genal_times, genal_fitns, '-', label='GenAl')
 plt.plot(mimic_times, mimic_fitns, '-', label='MIMIC')
 plt.legend(loc=2)
