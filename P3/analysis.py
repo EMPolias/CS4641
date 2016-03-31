c942e6cd4e0481941afacb05dff92cd455c7912f
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2016-03-31 15:05:59 -0400

    remove adult things from analysis, add apartment converter

--- P3/analysis.py
+++ P3/analysis.py
@@ -25,32 +25,10 @@ from pybrain.supervised.trainers import BackpropTrainer
 def create_mapper(l):
     return {l[n] : n for n in xrange(len(l))}
 
-workclass = create_mapper(["Private", "Self-emp-not-inc", "Self-emp-inc", "Federal-gov", "Local-gov", "State-gov", "Without-pay", "Never-worked"])
-education = create_mapper(["Bachelors", "Some-college", "11th", "HS-grad", "Prof-school", "Assoc-acdm", "Assoc-voc", "9th", "7th-8th", "12th", "Masters", "1st-4th", "10th", "Doctorate", "5th-6th", "Preschool"])
-marriage = create_mapper(["Married-civ-spouse", "Divorced", "Never-married", "Separated", "Widowed", "Married-spouse-absent", "Married-AF-spouse"])
-occupation = create_mapper(["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv", "Armed-Forces"])
-relationship = create_mapper(["Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"])
-race = create_mapper(["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"])
-sex = create_mapper(["Female", "Male"])
-country = create_mapper(["United-States", "Cambodia", "England", "Puerto-Rico", "Canada", "Germany", "Outlying-US(Guam-USVI-etc)", "India", "Japan", "Greece", "South", "China", "Cuba", "Iran", "Honduras", "Philippines", "Italy", "Poland", "Jamaica", "Vietnam", "Mexico", "Portugal", "Ireland", "France", "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti", "Columbia", "Hungary", "Guatemala", "Nicaragua", "Scotland", "Thailand", "Yugoslavia", "El-Salvador", "Trinadad&Tobago", "Peru", "Hong", "Holand-Netherlands"])
-income = create_mapper(["<=50K", ">50K"])
-
-adultDataSetConverters = {
-    1: lambda x: workclass[x],
-    3: lambda x: education[x],
-    5: lambda x: marriage[x],
-    6: lambda x: occupation[x],
-    7: lambda x: relationship[x],
-    8: lambda x: race[x],
-    9: lambda x: sex[x],
-    13: lambda x: country[x],
-    14: lambda x: income[x]
-}
-
-hillsDataSetConverters = {}
 SentimentDataSetConverters = {}
+ApartmentDataSetConverters = {}
 
-converters = {"adult": adultDataSetConverters, "hill": hillsDataSetConverters, "sentiment": SentimentDataSetConverters}
+converters = {"sentiment": SentimentDataSetConverters, "apartments": ApartmentDataSetConverters}
 
 
 def load(filename, converter):
